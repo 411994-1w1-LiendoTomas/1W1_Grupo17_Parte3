8b978fab2d2e30597f3b681394ffd7278bed24f7
Tomás Liendo <tomasliendo00@gmail.com>
2025-11-05 16:42:00 -0300

    Agregado GestorBD Terminado FormTomasLiendo

--- TPI-BDI-Grupo17/FormTomasLiendo.Designer.cs
+++ TPI-BDI-Grupo17/FormTomasLiendo.Designer.cs
@@ -33,16 +33,18 @@
             this.dtpFechaDesde = new System.Windows.Forms.DateTimePicker();
             this.dtpFechaHasta = new System.Windows.Forms.DateTimePicker();
             this.btnEjecutar = new System.Windows.Forms.Button();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.dgvResultados = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResultados)).BeginInit();
             this.SuspendLayout();
             // 
             // lblFechaDesde
             // 
             this.lblFechaDesde.AutoSize = true;
-            this.lblFechaDesde.Location = new System.Drawing.Point(27, 26);
+            this.lblFechaDesde.Location = new System.Drawing.Point(36, 35);
+            this.lblFechaDesde.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblFechaDesde.Name = "lblFechaDesde";
-            this.lblFechaDesde.Size = new System.Drawing.Size(90, 16);
+            this.lblFechaDesde.Size = new System.Drawing.Size(72, 13);
             this.lblFechaDesde.TabIndex = 0;
             this.lblFechaDesde.Text = "Fecha desde:";
             this.lblFechaDesde.Click += new System.EventHandler(this.label1_Click);
@@ -50,61 +52,81 @@
             // lblFechaHasta
             // 
             this.lblFechaHasta.AutoSize = true;
-            this.lblFechaHasta.Location = new System.Drawing.Point(33, 65);
+            this.lblFechaHasta.Location = new System.Drawing.Point(41, 67);
+            this.lblFechaHasta.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblFechaHasta.Name = "lblFechaHasta";
-            this.lblFechaHasta.Size = new System.Drawing.Size(84, 16);
+            this.lblFechaHasta.Size = new System.Drawing.Size(69, 13);
             this.lblFechaHasta.TabIndex = 1;
             this.lblFechaHasta.Text = "Fecha hasta:";
             // 
             // dtpFechaDesde
             // 
-            this.dtpFechaDesde.Location = new System.Drawing.Point(138, 21);
+            this.dtpFechaDesde.Location = new System.Drawing.Point(120, 31);
+            this.dtpFechaDesde.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dtpFechaDesde.Name = "dtpFechaDesde";
-            this.dtpFechaDesde.Size = new System.Drawing.Size(200, 22);
+            this.dtpFechaDesde.Size = new System.Drawing.Size(151, 20);
             this.dtpFechaDesde.TabIndex = 2;
             this.dtpFechaDesde.Value = new System.DateTime(2025, 11, 5, 14, 1, 4, 0);
             // 
             // dtpFechaHasta
             // 
-            this.dtpFechaHasta.Location = new System.Drawing.Point(138, 65);
+            this.dtpFechaHasta.Location = new System.Drawing.Point(120, 67);
+            this.dtpFechaHasta.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dtpFechaHasta.Name = "dtpFechaHasta";
-            this.dtpFechaHasta.Size = new System.Drawing.Size(200, 22);
+            this.dtpFechaHasta.Size = new System.Drawing.Size(151, 20);
             this.dtpFechaHasta.TabIndex = 3;
             this.dtpFechaHasta.Value = new System.DateTime(2025, 11, 5, 14, 1, 4, 0);
             // 
             // btnEjecutar
             // 
-            this.btnEjecutar.Location = new System.Drawing.Point(359, 20);
+            this.btnEjecutar.Location = new System.Drawing.Point(285, 30);
+            this.btnEjecutar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnEjecutar.Name = "btnEjecutar";
-            this.btnEjecutar.Size = new System.Drawing.Size(75, 23);
+            this.btnEjecutar.Size = new System.Drawing.Size(56, 19);
             this.btnEjecutar.TabIndex = 4;
             this.btnEjecutar.Text = "Ejecutar";
             this.btnEjecutar.UseVisualStyleBackColor = true;
+            this.btnEjecutar.Click += new System.EventHandler(this.btnEjecutar_Click);
             // 
-            // dataGridView1
+            // dgvResultados
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 105);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(776, 333);
-            this.dataGridView1.TabIndex = 5;
+            this.dgvResultados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvResultados.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dgvResultados.Location = new System.Drawing.Point(0, 109);
+            this.dgvResultados.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dgvResultados.Name = "dgvResultados";
+            this.dgvResultados.RowHeadersWidth = 51;
+            this.dgvResultados.RowTemplate.Height = 24;
+            this.dgvResultados.Size = new System.Drawing.Size(650, 271);
+            this.dgvResultados.TabIndex = 5;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(626, 92);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Consulta";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
             // 
             // FormTomasLiendo
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.dataGridView1);
+            this.ClientSize = new System.Drawing.Size(650, 380);
             this.Controls.Add(this.btnEjecutar);
             this.Controls.Add(this.dtpFechaHasta);
             this.Controls.Add(this.dtpFechaDesde);
             this.Controls.Add(this.lblFechaHasta);
             this.Controls.Add(this.lblFechaDesde);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dgvResultados);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "FormTomasLiendo";
             this.Text = "411994, Tomás Liendo";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.Load += new System.EventHandler(this.FormTomasLiendo_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResultados)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -117,6 +139,7 @@
         private System.Windows.Forms.DateTimePicker dtpFechaDesde;
         private System.Windows.Forms.DateTimePicker dtpFechaHasta;
         private System.Windows.Forms.Button btnEjecutar;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dgvResultados;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
